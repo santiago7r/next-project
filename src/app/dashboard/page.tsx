fb668baeb0c65dc69ff370e7c26d9acefad8c452
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-07-26 14:37:15 -0500

    Understanding layout in next

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,13 @@ import Link from "next/link";
 export default function Page() {
     return <>
         <h1>Hello, Dashboard Page!</h1>
+
+        <nav> 
+            <a href="/html/">HTML</a> |
+            <a href="/css/">CSS</a> |
+            <a href="/js/">JavaScript</a> |
+            <a href="/python/">Python</a>
+        </nav>
         <Link href="/">app</Link>
     </>
 }
