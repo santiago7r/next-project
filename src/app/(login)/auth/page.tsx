59bc1b88ed0c24b4b16ce3788513edb200a3ad6e
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-07-31 18:34:31 -0500

    using use client

--- src/app/(login)/auth/page.tsx
+++ src/app/(login)/auth/page.tsx
@@ -1,4 +1,20 @@
+'use client'
+
+import { useState } from "react"
+
 export default function Login() {
+    const [showComponent, setShowComponent] = useState('CLICK')
+
+    const handleOnClick = () => {
+        showComponent === 'CLICK' ? setShowComponent('ANOTHER THING') : setShowComponent('CLICK')
+    }
     
-    return <input placeholder="Name" />
+    return (
+        <div>
+            <button onClick={handleOnClick}>{showComponent}</button>
+        </div>
+        
+
+    )
+    
 }
