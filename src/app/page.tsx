0c8955fe5189c4015f3a84d7e3ff4d04a97484bf
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-08-30 15:19:46 -0500

    Use fonts suing next/fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
+import { Nerko_One, Roboto, Sevillana } from "next/font/google";
 import Link from "next/link";
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
+const nerko_one = Nerko_One({
+  subsets: ['latin'],
+  weight: ['400']
+})
+const sevillana = Sevillana({
+  subsets: ['latin'],
+  weight: ['400']
+})
 
 
 export default function Page() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
-      <Link href="/dashboard">Dashboard</Link>
+      <h1 className={roboto.className}>Hello, Next.js!</h1>
+      <Link className={nerko_one.className} href="/dashboard">Dashboard</Link>
       <div></div>
-      <Link href="/video">Video</Link>
+      <Link className={sevillana.className} href="/video">Video</Link>
     </>
   )
 }
